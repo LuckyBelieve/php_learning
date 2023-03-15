c1a2bc62d2f410d58e703c157a0b969520a3ae19
LuckyBelieve <luckybelieve10@gmail.com>
2023-03-15 21:10:41 +0200

    about page

---
+++ about.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>about page</title>
+</head>
+<body>
+    <h1>this is the about page</h1>
+    <p>welcome to the about page</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elitmaxime illum
+         eaque, alias odit, sit dolorem excepturi esse sed, nostrumarchitecto 
+        atque laboriosam minima quaerat. Rerum laudantium culpa consequuntur.</p>
+</body>
+</html>
